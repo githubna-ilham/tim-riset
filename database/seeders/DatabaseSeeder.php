450afbe390c29c7c3550ad246cddf3e811c76908
aaidilz <aidil.233040059@gmail.com>
2024-04-27 13:10:49 +0700

    Update .env.example and DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,23 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        // AdminSeeder
-        // $this->call(AdminSeeder::class);
-        // CustomerSeeder
-        // $this->call(CustomerSeeder::class);
-        // kendaraanSeeder
-        // $this->call(KendaraanSeeder::class);
-        // KategoriSparepartSeeder
-        // $this->call(KategoriSparepartSeeder::class);
-        // SparepartSeeder
-        // $this->call(SparepartSeeder::class);
-        // TransaksiSeeder
+        $this->call(AdminSeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(KategoriSparepartSeeder::class);
+        $this->call(KendaraanSeeder::class);
+        $this->call(SparepartSeeder::class);
         $this->call(TransaksiSeeder::class);
     }
 }
